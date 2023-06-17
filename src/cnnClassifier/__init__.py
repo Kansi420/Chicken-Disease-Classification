6185d6442e6169cd291a60c726126bda969c8aeb
Kansi420 <kanasepradhumn@gmailcom>
2023-06-17 21:31:08 +0530

    completed Data Ingestion

--- src/cnnClassifier/__init__.py
+++ src/cnnClassifier/__init__.py
@@ -16,7 +16,7 @@ logging.basicConfig(
     handlers=[
         logging.FileHandler(log_filepath),
         logging.StreamHandler(sys.stdout)
-    ]
+    ] 
 )
 
 logger = logging.getLogger("cnnClassifierLogger")
